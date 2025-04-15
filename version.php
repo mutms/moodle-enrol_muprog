d52a3d8b15a2d8171013f4e955518d42822af33f
Petr Skoda <petr@mutms.org>
2025-04-15 18:32:35 +0200

    Release mu-4.5.4-02

--- version.php
+++ version.php
@@ -28,8 +28,8 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'enrol_muprog';
-$plugin->version   = 2025041500;
+$plugin->version   = 2025041501;
 $plugin->requires  = 2024100700;
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->supported = [405, 405];
-$plugin->release   = 'mu-4.5.4-02';
+$plugin->release   = 'mu-4.5.4-03';
