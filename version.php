66e948a51e0094d8a9ecb00b0b1a68bed162f3ba
Petr Skoda <petr@mutms.org>
2025-05-01 06:53:06 +0200

    release mu-4.5.4-09

--- version.php
+++ version.php
@@ -32,4 +32,4 @@ $plugin->version   = 2025042900;
 $plugin->requires  = 2024100700;
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->supported = [405, 405];
-$plugin->release   = 'mu-4.5.4-08';
+$plugin->release   = 'mu-4.5.4-09';
